cbb24dddbb0db14962c8d64eff8f25fd38776fab
revlienn <revnathalia@gmail.com>
2025-09-30 14:28:44 +0100

    R - diy two step forms

--- src/app/login-reactive/login-reactive.ts
+++ src/app/login-reactive/login-reactive.ts
@@ -13,6 +13,9 @@ export class LoginReactive implements OnInit {
 
   initEmail:string='username@email.com';
   initPassword:string='password';
+  initUsername:string='username';
+  initMobilenumber:string='mobile number';
+  stepTwo:boolean=false;
 
   ngOnInit(): void {
     console.log(this.reactiveLoginForm.value);
@@ -25,25 +28,42 @@ export class LoginReactive implements OnInit {
   password = new FormControl(this.initPassword, {
     validators: [Validators.required, Validators.minLength(8),passwordStrengthValidator()]
   })
+  username=new FormControl(this.initUsername,{
+    validators:[Validators.required, Validators.minLength(3)]
+  })
+
+  mobileNumber=new FormControl(this.initMobilenumber,{
+    validators:[Validators.minLength(7)]
+  })
 
   reactiveLoginForm = new FormGroup({
     email: this.email,
-    password: this.password
+    password: this.password,
+    username:this.username,
+    mobileNumber:this.mobileNumber
   })
+
+  toggleStep(){
+    this.stepTwo=!this.stepTwo;
+  }
 
   reset(){
     this.reactiveLoginForm.reset({
       email:this.initEmail,
-      password:this.initPassword
+      password:this.initPassword,
+      username:this.initUsername,
+      mobileNumber:this.initMobilenumber
     });
     console.log(this.reactiveLoginForm.value);
   }
 
   login(){
-    console.log(this.reactiveLoginForm.value);
+   console.log(this.reactiveLoginForm.value);
     this.reactiveLoginForm.reset({
       email:'',
-      password:''
+      password:'',
+      username:'',
+      mobileNumber:''
     });
   }
 }
