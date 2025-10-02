bc114a8ec40587583a2867636365434df45f4cf0
revlienn <revnathalia@gmail.com>
2025-10-02 11:05:53 +0100

    Router - home and wildcard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { LoginReactive } from './login-reactive/login-reactive';
 import { Login } from './login/login';
 import { Registration } from './registration/registration';
 import { FoodModule } from './food/food-module';
+import { PageNotFound } from './page-not-found/page-not-found';
 
 export const routes: Routes = [
+   {path:'',redirectTo:'login',pathMatch:'full'},
    {path:'login',component:Login},
    {path:'login-reactive',component:LoginReactive},
    {path:'register',component:Registration},
+   {path:'**',component:PageNotFound}
    // {
    //    path:'food',
    //    loadChildren:()=>import('./food/food-module').then((m)=>m.FoodModule)
