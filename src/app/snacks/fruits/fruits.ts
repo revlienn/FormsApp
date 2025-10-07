9b0565253446f737ccbca3bf7ad146ba286df5de
revlienn <revnathalia@gmail.com>
2025-10-07 10:55:37 +0100

    rxjs-map, tap

--- src/app/snacks/fruits/fruits.ts
+++ src/app/snacks/fruits/fruits.ts
@@ -1,9 +1,10 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { filter, from, fromEvent, interval, Observable, of, Subject, Subscription, timer } from 'rxjs';
+import { filter, map, of, Subject, tap } from 'rxjs';
+import { ajax } from 'rxjs/ajax'
 
-type News={
-  category:"Business"|"Sports";
-  content:string;
+type News = {
+  category: "Business" | "Sports";
+  content: string;
 }
 
 @Component({
@@ -16,22 +17,27 @@ export class Fruits implements OnInit {
 
   private destroy$ = new Subject<void>();
 
-  newsFeed=new Observable<News>((s)=>{
-    setTimeout(()=>{
-      s.next({category:'Business',content:'News item #1'})
-    },1000)
-    setTimeout(()=>{
-      s.next({category:'Business',content:'News item #2'})
-    },2500)
-    setTimeout(()=>{
-      s.next({category:'Sports',content:'News item #3'})
-    },3500)
-  })
-
   ngOnInit(): void {
-    this.newsFeed
-      .pipe(filter((r)=>r.category=='Sports'))
-      .subscribe((r)=>console.log(r));
+
+
+    const userData$ = ajax<any>("https://randomuser.me/api");
+
+    userData$.pipe(map((user) => ({
+      firstName: user.response.results[0].name.first,
+      lastName: user.response.results[0].name.last,
+      country: user.response.results[0].location.country
+    }))).subscribe(({ firstName, lastName, country }) => {
+      console.log(firstName, lastName, '-',country)
+    })
+
+    userData$.subscribe((r) => console.log(r))
+
+    of(1,23,7,10).pipe(
+      filter((num)=>num>5),
+      tap((filtered)=>console.log("🕵️‍♀️SPY ",filtered)),
+      map((spied)=>spied*2)
+    ).subscribe((v)=>console.log(`TIMES 2: ${v}`))
+    
   }
 
   ngOnDestroy(): void {
@@ -40,3 +46,4 @@ export class Fruits implements OnInit {
   }
 
 }
+
