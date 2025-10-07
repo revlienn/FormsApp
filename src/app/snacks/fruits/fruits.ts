db93fea9ace64a86470af41a95a8a86c4e70e25e
revlienn <revnathalia@gmail.com>
2025-10-07 11:45:19 +0100

    rxjs-switchMap, mergeMap, concatMap

--- src/app/snacks/fruits/fruits.ts
+++ src/app/snacks/fruits/fruits.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { catchError, combineLatest, concat, EMPTY, filter, from, interval, map, Observable, of, Subject, takeUntil, tap, zip } from 'rxjs';
+import { catchError, combineLatest, concat, concatMap, EMPTY, filter, from, fromEvent, interval, map, mergeMap, Observable, of, Subject, switchMap, take, takeUntil, tap, zip } from 'rxjs';
 import { ajax } from 'rxjs/ajax'
 
 type News = {
@@ -16,20 +16,32 @@ type News = {
 export class Fruits implements OnInit {
 
   private destroy$ = new Subject<void>();
+  clicks$=fromEvent(document,'click');
+  numbers$=interval(500).pipe(take(5));
+  
   ngOnInit(): void {
 
-    interval(1000)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((v)=>{
-        console.log('Hello World')
-      })
+    // this.clicks$.pipe(
+    //   switchMap(()=>this.numbers$)
+    // ).subscribe(
+    //   val=>console.log(`SWITCHMAP Click: ${val}`)
+    // )
 
-    setTimeout(()=>this.destroy$.next(),3000)
+    // this.clicks$.pipe(
+    //   mergeMap(()=>this.numbers$)
+    // ).subscribe(
+    //   val=>console.log(`MERGE MAP, ${val}`)
+    // )
+
+    this.clicks$.pipe(
+      concatMap(()=>this.numbers$)
+    ).subscribe(val=>console.log(val))
+
   }
 
   ngOnDestroy(): void {
-    // this.destroy$.next();
-    // this.destroy$.complete();
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
 }
