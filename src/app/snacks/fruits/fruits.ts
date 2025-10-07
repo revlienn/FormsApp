7e3e14bfa79e27c31ad16dbb7852a50c6b7f70ad
revlienn <revnathalia@gmail.com>
2025-10-07 09:31:34 +0100

    rxjs - of and from

--- src/app/snacks/fruits/fruits.ts
+++ src/app/snacks/fruits/fruits.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subject, Subscription } from 'rxjs';
+import { from, Observable, of, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-fruits',
@@ -13,7 +13,8 @@ export class Fruits implements OnInit {
   private destroy$ = new Subject<void>();
 
   ngOnInit(): void {
-  
+    of('Apple').subscribe((res)=>console.log(res));
+    from(['Banana','Cherry']).subscribe((res)=>console.log(res));
   }
 
   ngOnDestroy(): void {
