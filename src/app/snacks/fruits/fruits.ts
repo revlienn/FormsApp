d6881b8ee36a398c662552ddf79fa19d135d498d
revlienn <revnathalia@gmail.com>
2025-10-07 10:39:57 +0100

    rxjs-filter

--- src/app/snacks/fruits/fruits.ts
+++ src/app/snacks/fruits/fruits.ts
@@ -1,5 +1,10 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { from, fromEvent, interval, Observable, of, Subject, Subscription, timer } from 'rxjs';
+import { filter, from, fromEvent, interval, Observable, of, Subject, Subscription, timer } from 'rxjs';
+
+type News={
+  category:"Business"|"Sports";
+  content:string;
+}
 
 @Component({
   selector: 'app-fruits',
@@ -7,25 +12,26 @@ import { from, fromEvent, interval, Observable, of, Subject, Subscription, timer
   styleUrl: './fruits.css',
   standalone: false
 })
-export class Fruits implements OnInit,AfterViewInit {
-
-  @ViewChild('fromEventBtn') fromEventBtn!:ElementRef<HTMLButtonElement>;
+export class Fruits implements OnInit {
 
   private destroy$ = new Subject<void>();
 
-
-  ngAfterViewInit(): void {
-    timer(3000).subscribe(()=>{
-      const btn=this.fromEventBtn.nativeElement;
-      btn.classList.remove('btn-outline');
-      btn.classList.add('btn-primary')
-    })
-
-    const intervalVar$=interval(1000).subscribe(()=>console.log('interval 🎁'));
-    setTimeout(()=>{intervalVar$.unsubscribe()},3000)
-  }
+  newsFeed=new Observable<News>((s)=>{
+    setTimeout(()=>{
+      s.next({category:'Business',content:'News item #1'})
+    },1000)
+    setTimeout(()=>{
+      s.next({category:'Business',content:'News item #2'})
+    },2500)
+    setTimeout(()=>{
+      s.next({category:'Sports',content:'News item #3'})
+    },3500)
+  })
 
   ngOnInit(): void {
+    this.newsFeed
+      .pipe(filter((r)=>r.category=='Sports'))
+      .subscribe((r)=>console.log(r));
   }
 
   ngOnDestroy(): void {
