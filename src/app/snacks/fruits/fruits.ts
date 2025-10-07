dc683db7b3b10c6b1dfeb4a5f89e47172b218891
revlienn <revnathalia@gmail.com>
2025-10-07 09:15:21 +0100

    rxjs - empty and value emit subscription

--- src/app/snacks/fruits/fruits.ts
+++ src/app/snacks/fruits/fruits.ts
@@ -5,30 +5,44 @@ import { Observable, Subject, Subscription } from 'rxjs';
   selector: 'app-fruits',
   templateUrl: './fruits.html',
   styleUrl: './fruits.css',
-  standalone:false
+  standalone: false
 })
-export class Fruits implements OnInit{
+export class Fruits implements OnInit {
 
-  fruits$=new Observable<string>((subscriber)=>{
+  fruits$ = new Observable<string>((subscriber) => {
     subscriber.next('Apple');
     subscriber.next('Banana');
     subscriber.next('Cherry');
     subscriber.complete();
   })
 
-  private destroy$=new Subject<void>();
+  emptylog$ = new Observable((s) => {
+    s.next('empty log');
+    s.complete();
+  })
+
+  timerLog$ = new Observable((s) => {
+    s.next('value');
+    setTimeout(() => {
+      s.next('timer log');
+      s.complete();
+    }, 1500)
+    return(()=>{
+      console.log('Teardown')
+    })
+  })
+
+  private destroy$ = new Subject<void>();
 
   ngOnInit(): void {
-    this.fruits$.subscribe({
-      next:(res:string)=>console.log(res),
-      error:(err:Error)=>console.error(err.message),
-      complete:()=>console.log('complete 🔥')
+    this.emptylog$.subscribe();
+    this.timerLog$.subscribe((s) => {
+      console.log(`${s}`)
     })
-
-    this.fruits$.subscribe((res)=>console.log(`2-${res}`))
+  
   }
 
-  ngOnDestroy():void{
+  ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
     console.log('destroyed')
