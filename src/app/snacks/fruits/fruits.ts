e3bc8b50dd1ee09e19b6e456a98ad8274db49650
revlienn <revnathalia@gmail.com>
2025-10-08 10:09:52 +0100

    rxjs-distinct, distinctUntilChanged

--- src/app/snacks/fruits/fruits.ts
+++ src/app/snacks/fruits/fruits.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { catchError, combineLatest, concat, concatMap, EMPTY, filter, from, fromEvent, interval, map, merge, mergeMap, Observable, of, scan, startWith, Subject, switchMap, take, takeUntil, tap, withLatestFrom, zip } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, OnInit, signal, ViewChild } from '@angular/core';
+import { catchError, combineLatest, concat, concatMap, debounceTime, distinct, distinctUntilChanged, EMPTY, filter, from, fromEvent, interval, map, merge, mergeMap, Observable, of, scan, startWith, Subject, switchMap, take, takeUntil, tap, withLatestFrom, zip } from 'rxjs';
 import { ajax } from 'rxjs/ajax'
 
 type News = {
@@ -16,33 +16,36 @@ type News = {
 })
 export class Fruits implements AfterViewInit {
 
-  @ViewChild('countBtn') countBtn!:ElementRef<HTMLButtonElement>;
-  @ViewChild('showCalcBtn') showCalcBtn!:ElementRef<HTMLButtonElement>;
+  @ViewChild('inputField') inputField!:ElementRef<HTMLInputElement>;
 
   private destroy$ = new Subject<void>();
 
-  location:{x:number,y:number}[]=[];
-  time:number[]=[];
+  distinctUntilChangedRecs:string[]=[];
+  distinctRecs:string[]=[];
   
   ngAfterViewInit(): void {
+    fromEvent(this.inputField.nativeElement,'input').pipe(
+      map(()=>this.inputField.nativeElement.value),
+      debounceTime(500),
+      distinctUntilChanged(),
+    )
+    .subscribe(
+      (res)=>{
+        this.distinctUntilChangedRecs.push(res);
+        console.log(`${res}-${this.distinctUntilChangedRecs}`)
+      }
+    )
 
-    fromEvent(this.countBtn.nativeElement,'click').pipe(
-      scan(count=>count+1,0)
-    ).subscribe((count)=>console.log(`Tally: `,count,` times`))
-
-    fromEvent<MouseEvent>(this.showCalcBtn.nativeElement,'click').pipe(
-      scan((acc,curr)=>{
-        const currentLocation={x:curr.clientX,y:curr.clientY};
-        const currentTime=curr.timeStamp;
-        this.location.push(currentLocation);
-        this.time.push(currentTime);
-        return acc+1
-      },0))
-      .subscribe((count)=>{
-        console.log(`Calc: `,count,` times`);
-        console.log(this.location, this.time);
-      })
-
+    //  fromEvent(this.inputField.nativeElement,'input').pipe(
+    //   map(()=>this.inputField.nativeElement.value),
+    //   distinct(),
+    // )
+    // .subscribe(
+    //   (res)=>{
+    //     this.distinctRecs.push(res);
+    //     console.log(`${res}-${this.distinctRecs}`)
+    //   }
+    // )
   }
 
   ngOnDestroy(): void {
